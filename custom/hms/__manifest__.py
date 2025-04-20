5899cb67f4a3ed5c0ca917561f1d6398224d0425
PyMustafa <proo.eng2018@gmail.com>
2025-04-20 22:24:39 +0200

    first commit

---
+++ custom/hms/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "HMS",
+    "version": "1.0",
+    "summary": "Hospital Management System",
+    "category": "Healthcare",
+    "author": "Your Name",
+    "depends": ["base"],
+    "description": """
+        Hospital management system module.
+    """,
+    "data": [
+        "views/patient_views.xml",
+        "views/doctor_views.xml",
+        "views/department_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
